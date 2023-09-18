bc9500ccf245d73a682162f9b5654245e6272795
Liviu Hariton <liviu.hariton@gmail.com>
2023-09-18 20:38:12 +0300

    Layout styling

--- resources/views/tasks/list.blade.php
+++ resources/views/tasks/list.blade.php
@@ -7,7 +7,8 @@
         <ul class="list-group list-group-flush">
         @foreach($tasks as $task)
             <li class="list-group-item">
-                <a href="{{ route('tasks.details', ['task' => $task->id]) }}" class="text-dark">{{ $task->title }}</a> <a href="{{ route('tasks.destroy', ['task' => $task->id]) }}" title="Delete task" class="text-danger"><i class="fa fa-trash-alt"></i></a>
+                <a href="{{ route('tasks.details', ['task' => $task->id]) }}" @class(['text-dark' => !$task->completed, 'text-decoration-line-through text-success' => $task->completed])>{{ $task->title }}</a>
+                <a href="{{ route('tasks.destroy', ['task' => $task->id]) }}" title="Delete task" class="text-danger"><i class="fa fa-trash-alt"></i></a>
             </li>
         @endforeach
         </ul>
